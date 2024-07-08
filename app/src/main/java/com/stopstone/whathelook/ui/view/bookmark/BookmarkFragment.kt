1cca7bc887977e5bad8878e48f05c9f2b1a9c527
StopStone <wltjr6432@naver.com>
2024-07-08 18:04:11 +0900

    feat: 메인 bottom Navigation 구성

---
+++ app/src/main/java/com/stopstone/whathelook/ui/view/bookmark/BookmarkFragment.kt
@@ -0,0 +1,30 @@
+package com.stopstone.whathelook.ui.view.bookmark
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.stopstone.whathelook.databinding.FragmentBookmarkBinding
+
+class BookmarkFragment : Fragment() {
+    private var _binding: FragmentBookmarkBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View {
+        _binding = FragmentBookmarkBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+    }
+}
