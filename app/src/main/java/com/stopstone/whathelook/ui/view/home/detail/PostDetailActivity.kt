c0d8b661fb664f02a6259b0ef1f6c033de569f9f
stopstone <wltjr6432@naver.com>
2024-07-22 06:33:05 +0900

    feat: 상세 페이지 구성

--- app/src/main/java/com/stopstone/whathelook/ui/view/home/detail/PostDetailActivity.kt
+++ app/src/main/java/com/stopstone/whathelook/ui/view/home/detail/PostDetailActivity.kt
@@ -2,13 +2,8 @@ package com.stopstone.whathelook.ui.view.home.detail
 
 import android.os.Bundle
 import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import androidx.navigation.navArgs
 import com.bumptech.glide.Glide
-import com.stopstone.whathelook.R
 import com.stopstone.whathelook.data.model.Post
 import com.stopstone.whathelook.databinding.ActivityPostDetailBinding
 
