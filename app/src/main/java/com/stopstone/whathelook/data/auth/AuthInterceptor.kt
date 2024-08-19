20fe4070ad09a45065dd3126a34928d6c69a47fb
StopStone <wltjr6432@naver.com>
2024-08-20 06:17:30 +0900

    401에러 시 리프레쉬 토큰으로 재요청

--- app/src/main/java/com/stopstone/whathelook/data/auth/AuthInterceptor.kt
+++ app/src/main/java/com/stopstone/whathelook/data/auth/AuthInterceptor.kt
@@ -1,26 +1,56 @@
 package com.stopstone.whathelook.data.auth
 
+import android.content.Context
+import android.content.Intent
 import android.util.Log
 import com.stopstone.whathelook.data.local.TokenManager
+import com.stopstone.whathelook.view.login.LogInActivity
+import kotlinx.coroutines.runBlocking
 import okhttp3.Interceptor
 import okhttp3.Response
 import javax.inject.Inject
 
-class AuthInterceptor @Inject constructor(private val tokenManager: TokenManager) : Interceptor {
+class AuthInterceptor @Inject constructor(
+    private val tokenManager: TokenManager,
+) : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
         val originalRequest = chain.request()
         val accessToken = tokenManager.getAccessToken()
 
-        val newRequest = if (!accessToken.isNullOrEmpty()) {
-            Log.d("AuthInterceptor", "Adding Authorization Header: Bearer ${accessToken.take(10)}...")
+        val request = if (!accessToken.isNullOrEmpty()) {
             originalRequest.newBuilder()
                 .header("Authorization", "Bearer $accessToken")
                 .build()
         } else {
-            Log.w("AuthInterceptor", "No access token available")
             originalRequest
         }
 
-        return chain.proceed(newRequest)
+        var response = chain.proceed(request)
+
+        if (response.code == 401) {
+            Log.d("AuthInterceptor", "Received 401 error. Attempting to refresh token.")
+            response.close()
+
+            val newAccessToken = runBlocking {
+                refreshToken()
+            }
+
+            return if (newAccessToken != null) {
+                // 새 토큰으로 요청 재시도
+                chain.proceed(
+                    originalRequest.newBuilder()
+                        .header("Authorization", "Bearer $newAccessToken")
+                        .build()
+                )
+            } else {
+                response
+            }
+        }
+
+        return response
+    }
+
+    private fun refreshToken(): String? {
+        return tokenManager.getRefreshToken()
     }
 }
